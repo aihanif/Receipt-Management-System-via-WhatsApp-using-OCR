dd2bea51a0fef31c770fd2bcc11253e83797471b
HanifWahab <hanifh82@yahoo.com>
2025-06-26 21:34:22 +0800

    Update submit_add.php
    update script

--- submit_add.php
+++ submit_add.php
@@ -1,9 +1,5 @@
 <?php
 // submit_add.php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 
 // Connect to Database
 require_once 'connection_work.php';
